285cac3b6db412660f090af16971b8cac0cdd12d
Monis9451 <mianmonishussain786@gmail.com>
2024-07-30 17:50:15 +0500

    added insertAtHead and insertAtTail function

--- Linked list/initial_practice.cpp
+++ Linked list/initial_practice.cpp
@@ -1,36 +1,59 @@
 #include <iostream>
 using namespace std;
 
-class Node{
+class Node
+{
 public:
     int data;
-    Node* next;
+    Node *next;
 
-    Node(){
+    Node()
+    {
         data = 0;
         next = NULL;
     }
 
-    Node(int data){
-        this-> data = data;
-        this-> next = NULL;
-    }
-    void display(){
-        cout<<this->data<<endl;
-        cout<<this->next<<endl;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
     }
 };
 
-void insertAtHead(Node* &head, int d){
-    Node* temp = new Node(d);
+void insertAtHead(Node *&head, int d)
+{
+    Node *temp = new Node(d);
     temp->next = head;
     head = temp;
 }
 
+
+void insertAtTail(Node *&tail, int d){
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
+}
+
+void display(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
 int main()
 {
-    Node* node = new Node(10);
-    node->display();
+    Node *node = new Node(10);
+    Node* head = node;
+    Node* tail = node;
 
+    display(tail);
+    insertAtTail(head,12);
+    display(tail);
+    insertAtTail(head,15);
+    display(tail);
     return 0;
 }
