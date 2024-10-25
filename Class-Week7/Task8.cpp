a73fb9bbed6f3b551a2f2c761efef56f731a9bd5
Monis9451 <mianmonishussain786@gmail.com>
2024-10-26 03:31:51 +0500

    Insert At ending in a linkedlist recursively

---
+++ Class-Week7/Task8.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node *next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+Node* insertAtTail(Node *head, int value)
+{
+    if(head->next == nullptr)
+    {
+        Node *newNode = new Node(value);
+        head->next = newNode;
+        cout << "Data inserted at the end recursively." << endl;
+    }
+    else
+    {
+        insertAtTail(head->next, value);
+    }
+    return head;
+}
+
+void insertAtEnd(Node*& head, int data)
+{
+    if (head == nullptr)
+    {
+        head = new Node(data);
+        cout << "Data inserted at the end simply." << endl;
+    }
+    else
+    {
+        Node *newNode = new Node(data);
+        Node *temp = head;
+        while (temp->next != nullptr)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        cout << "Data inserted at the end simply." << endl;
+    }
+}
+
+void printList(Node *head)
+{
+    while (head != nullptr)
+    {
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+    cout << "nullptr" << endl;
+}
+
+int main()
+{
+    Node *head = nullptr;
+    insertAtEnd(head, 1);
+    insertAtEnd(head, 2);
+    insertAtEnd(head, 3);
+    printList(head);
+    head = insertAtTail(head, 4);
+    head = insertAtTail(head, 5);
+    printList(head);
+
+    return 0;
+}
